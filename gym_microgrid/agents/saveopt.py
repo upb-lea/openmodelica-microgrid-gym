fd165ec05d17c02651d9c19d54462a42e123c743
Stefan Heid <stefan.heid@upb.de>
2020-03-17 17:46:21 +0100

    initial ideas on observe function of the safeopt agent

--- gym_microgrid/agents/saveopt.py
+++ gym_microgrid/agents/saveopt.py
@@ -4,3 +4,13 @@ from gym_microgrid.agents import Agent
 class SafeOptAgent(Agent):
     def __init__(self):
         super().__init__()
+        self.episode_reward = 0
+
+    def observe(self, reward, terminated):
+        self.episode_reward += reward
+        if terminated:
+            # safeopt update step
+            # TODO
+            # reset episode reward
+            self.episode_reward = 0
+        # on other steps we don't need to do anything
