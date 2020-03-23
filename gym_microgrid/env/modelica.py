8200d992cf9412993742eb65b6a2522b50a90efa
Stefan Heid <stefan.heid@upb.de>
2020-03-23 13:56:00 +0100

    moved reward function into parameters of the environment

--- gym_microgrid/env/modelica.py
+++ gym_microgrid/env/modelica.py
@@ -5,7 +5,6 @@ from typing import Sequence
 
 import gym
 import numpy as np
-import pandas as pd
 import scipy
 from pyfmi import load_fmu
 from pyfmi.fmi import FMUModelME2
@@ -31,29 +30,17 @@ class ModelicaEnv(gym.Env):
         _get_action_space(), _get_observation_space(), _is_done()
     and provide required config, that describes your model.
 
-    Note:
-        Here is an example of config for the inverted pendulum problem:
-        config = {
-            'model_input_names': 'u',
-            'model_output_names': ['s', 'v', 'phi1', 'w'],
-            'model_parameters': {'m_trolley': 1, 'm_load': 0.1,
-                                 'phi1_start': 85/180*math.pi, 'w1_start': 0},
-            'time_step': 0.01,
-            'positive_reward': 1, # (optional)
-            'negative_reward': -100 # (optional)
-        }
 
     If you need more sophisticated logic to be implemented - override methods of OpenAI Gym API
-    in the child-class: reset(), step(), render(), close(), seed(), _reward_policy().
-    You can reuse get_state() and do_simulation() for faster development.
+    in the child-class: reset(), step(), render(), close()
 
     All methods called on model are from implemented PyFMI API.
     """
 
     viz_modes = {'episode', 'step', None}
 
-    def __init__(self, time_step: float = 1e-4, positive_reward: float = 1, negative_reward: float = -100,
-                 log_level: int = logging.WARNING, solver_method='LSODA', max_episode_steps: int = 10000.0,
+    def __init__(self, time_step: float = 1e-4, reward_fun: callable = lambda obs: 1,
+                 log_level: int = logging.WARNING, solver_method='LSODA', max_episode_steps: int = None,
                  model_params: dict = None,
                  model_input: Sequence[str] = None, model_output: Sequence[str] = None, model_path='grid.network.fmu',
                  time_start=0,
@@ -83,8 +70,7 @@ class ModelicaEnv(gym.Env):
         logger.debug("Successfully loaded model {}".format(model_name))
 
         # if you reward policy is different from just reward/penalty - implement custom step method
-        self.positive_reward = positive_reward
-        self.negative_reward = negative_reward
+        self.reward = reward_fun
 
         # Parameters required by this implementation
         self.time_start = time_start
@@ -125,9 +111,9 @@ class ModelicaEnv(gym.Env):
             e_info = self.model.get_event_info()
 
         self.model.enter_continuous_time_mode()
-        # list of keys and list of values
 
         if self.model_parameters is not None:
+            # list of keys and list of values
             self.model.set(*zip(*self.model_parameters.items()))
 
         # precalculating indices for more efficient lookup
@@ -137,16 +123,14 @@ class ModelicaEnv(gym.Env):
         return self
 
     def _calc_jac(self, t, x):
-        # t and x are indirectly retrieved from the model
-        states = self.model.get_states_list()
-        states_references = [s.value_reference for s in states.values()]
-        derivatives = self.model.get_derivatives_list()
-        derivatives_references = [d.value_reference for d in derivatives.values()]
-        self.jacobian = np.identity(len(states))
+        # get state and derivative value reference lists
+        refs = [[s.value_reference for s in getattr(self.model, attr).values()] for attr in
+                ['get_states_list', 'get_derivatives_list']]
+        jacobian = np.identity(len(refs[1]))
         np.apply_along_axis(
-            lambda col: self.model.get_directional_derivative(states_references, derivatives_references, col), 0,
-            self.jacobian)
-        return self.jacobian
+            lambda col: self.model.get_directional_derivative(*refs, col), 0,
+            jacobian)
+        return jacobian
 
     def _get_deriv(self, t, x):
         self.model.time = t
@@ -169,7 +153,6 @@ class ModelicaEnv(gym.Env):
         x_0 = self.model.continuous_states
 
         # Get the output from a step of the solver
-
         sol_out = scipy.integrate.solve_ivp(
             self._get_deriv, self.sim_time_interval, x_0, method=self.solver_method, jac=self._calc_jac)
         # get the last solution of the solver
@@ -178,16 +161,6 @@ class ModelicaEnv(gym.Env):
         obs = np.array(self.model.continuous_states)[self.model_output_index]
         self.history.append(obs)
         return obs
-
-    @property
-    def reward(self):
-        """
-        Determines reward based on the current environment state.
-        By default, implements simple logic of penalizing for experiment end and rewarding each step.
-
-        :return: reward associated with the current state
-        """
-        return self.negative_reward or -100 if self.is_done else self.positive_reward or 1
 
     @property
     def is_done(self) -> bool:
@@ -235,7 +208,7 @@ class ModelicaEnv(gym.Env):
                 """You are calling 'step()' even though this environment has already returned done = True.
                 You should always call 'reset()' once you receive 'done = True' -- any further steps are
                 undefined behavior.""")
-            return self.state, self.negative_reward, self.is_done
+            return self.state, None, self.is_done
 
         # check if action is a list. If not - create list of length 1
         try:
@@ -260,9 +233,8 @@ class ModelicaEnv(gym.Env):
         self.state = self._simulate()
 
         logger.debug("model output: {}, values: {}".format(flatten(self.model_output_names), self.state))
-        # print("model output: {}, values: {}".format(self.model_output_names, self.state))
-        # Check if experiment has finished
 
+        # Check if experiment has finished
         # Move simulation time interval if experiment continues
         if not self.is_done:
             logger.debug("Experiment step done, experiment continues.")
@@ -270,7 +242,7 @@ class ModelicaEnv(gym.Env):
         else:
             logger.debug("Experiment step done, experiment done.")
 
-        return self.state, self.reward, self.is_done
+        return self.state, self.reward(self.state), self.is_done
 
     def render(self, mode='human', close=False):
         """
