8e39e47f0f0c187105314b248e4a1a6aca5ebb93
hbode <bode@lea.uni-paderborn.de>
2020-03-23 18:31:19 +0100

    loadswitch added, throws errors

--- examples/model_params.py
+++ examples/model_params.py
@@ -2,10 +2,14 @@ import gym
 from gym_microgrid.agents import SafeOptAgent
 from gym_microgrid import Runner
 
+def f(t):
+    return 0 if t < 0.2 else 1
+
 if __name__ == '__main__':
     agent = SafeOptAgent()
     env = gym.make('gym_microgrid:ModelicaEnv_test-v1',
                    viz_mode='episode',
+                   model_params={'rl1.switch1.state': f,'rl1.switch2.state': f, 'rl1.switch3.state': f},
                    model_input=['i1p1', 'i1p2', 'i1p3', 'i2p1', 'i2p2', 'i2p3'],
                    model_output={
                        'lc1': [
