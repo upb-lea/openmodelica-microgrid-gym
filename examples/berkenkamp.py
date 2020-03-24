8bd68c11aadc205c03141af36b0c2267d7948a04
Stefan Heid <stefan.heid@upb.de>
2020-03-24 16:05:05 +0100

    #12 changed variable lookup in environment to be able to read more internal variables

--- examples/berkenkamp.py
+++ examples/berkenkamp.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     agent = SafeOptAgent()
     env = gym.make('gym_microgrid:ModelicaEnv_test-v1',
                    viz_mode='episode',
+                   model_path='grid.pll.fmu',
                    model_input=['i1p1', 'i1p2', 'i1p3', 'i2p1', 'i2p2', 'i2p3'],
                    model_output={
                        'lc1': [
@@ -14,9 +15,8 @@ if __name__ == '__main__':
                        'lcl1':
                            [['inductor1.i', 'inductor2.i', 'inductor3.i'],
                             ['capacitor1.v', 'capacitor2.v', 'capacitor3.v']],
-                       'pll':
-                           ['pi.x', 'f2theta.y']
-                   })
+                       'pll': ['add_freq_nom_delta_f.y']}
+                   )
 
     runner = Runner(agent, env)
     runner.run(1, visualize=True)
