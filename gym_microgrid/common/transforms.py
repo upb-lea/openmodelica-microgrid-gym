fixed inst_reactivePower calculation using np.roll updated testing refdata

gym_microgrid/common/transforms.py
@@ -146,5 +146,5 @@ def inst_power(varr: np.ndarray, iarr: np.ndarray):
 
 
 def inst_reactive(varr: np.ndarray, iarr: np.ndarray):
-    vline = np.array([varr[1] - varr[2], varr[2] - varr[0], varr[0] - varr[1]])
-    return -0.5773502691896258 * vline @ iarr
+    # vline = np.array([varr[1] - varr[2], varr[2] - varr[0], varr[0] - varr[1]]) # Linevoltages cal using np.roll
+    return -0.5773502691896258 * (np.roll(varr, -1) - np.roll(varr, -2)) @ iarr

tests/test_transforms.py
@@ -9,7 +9,7 @@ def seed():
 
 
 def test_inst_reactive(seed):
-    assert inst_reactive(np.random.random(3), np.random.random(3)) == approx(-0.14795581512942466)
+    assert inst_reactive(np.random.random(3), np.random.random(3)) == approx(-0.07421999471678885)
 
 
 def test_inst_power(seed):