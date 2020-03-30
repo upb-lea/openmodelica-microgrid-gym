cc94cb57e6c51b01a74d42f99263a3f8717829c7
Stefan Heid <stefan.heid@upb.de>
2020-03-30 09:45:49 +0200

    #8 implemented parameter updates and resets in safeopt agent

--- examples/berkenkamp.py
+++ examples/berkenkamp.py
@@ -60,6 +60,7 @@ if __name__ == '__main__':
 
     env = gym.make('gym_microgrid:ModelicaEnv_test-v1',
                    viz_mode='episode',
+                   max_episode_steps=100,
                    model_path='grid.pll.fmu',
                    model_input=['i1p1', 'i1p2', 'i1p3', 'i2p1', 'i2p2', 'i2p3'],
                    model_output=dict(lc1=[['inductor1.i', 'inductor2.i', 'inductor3.i'],
--- gym_microgrid/agents/safeopt.py
+++ gym_microgrid/agents/safeopt.py
@@ -1,12 +1,11 @@
-from typing import Dict, Union, Tuple
+from typing import Dict, Union
 
 import GPy
-import pandas as pd
-from GPy import Parameterized
 from GPy.kern import Kern
 from safeopt import SafeOptSwarm
 
 from gym_microgrid.agents.staticctrl import StaticControlAgent
+from gym_microgrid.agents.util import MutableParams
 from gym_microgrid.controllers import Controller
 
 import numpy as np
@@ -15,10 +14,10 @@ import numpy as np
 class SafeOptAgent(StaticControlAgent):
     def __init__(self, mutable_params: Union[dict, list], kernel: Kern, ctrls: Dict[str, Controller],
                  observation_action_mapping: dict):
-        self.default_params = list(mutable_params.values()) if isinstance(mutable_params, dict) else mutable_params
+        self.params = MutableParams(
+            list(mutable_params.values()) if isinstance(mutable_params, dict) else mutable_params)
         self.kernel = kernel
 
-        self.params = None
         self.score = None
         self.optimizer = None
         super().__init__(ctrls, observation_action_mapping)
@@ -28,9 +27,7 @@ class SafeOptAgent(StaticControlAgent):
         # self.kernel = type(self.kernel)(**self.kernel.to_dict())
         self.kernel = GPy.kern.Matern32(input_dim=1, lengthscale=.01)
 
-        print(np.array(self.default_params))
-        # TODO: correctly copy the parameters
-        self.params = self.default_params.copy()
+        self.params.reset()
         self.score = None
         self.optimizer = None
         return super().reset()
@@ -49,13 +46,12 @@ class SafeOptAgent(StaticControlAgent):
         if self.optimizer is None:
             bounds = [(-0.1, 0.2)]
             noise_var = 0.05 ** 2
-            gp = GPy.models.GPRegression(np.array([[self.params]]), np.array([[self.score]]), self.kernel,
+            gp = GPy.models.GPRegression(np.array([self.params[:]]), np.array([[self.score]]), self.kernel,
                                          noise_var=noise_var)
             self.optimizer = SafeOptSwarm(gp, 0., bounds=bounds, threshold=1)
         else:
-            # TODO correctly change the parameters
-            self.optimizer.add_new_data_point(self.params, self.score)
-        self.params = self.optimizer.optimize()
+            self.optimizer.add_new_data_point(self.params[:], self.score)
+        self.params[:] = self.optimizer.optimize()
 
     def render(self):
         # TODO plot the GP
