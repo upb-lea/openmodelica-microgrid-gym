9e9d8f5fc721dccf14a5b2c467ca7ca54b9e22bb
Stefan Heid <stefan.heid@upb.de>
2020-03-31 15:46:18 +0200

    minor changes on agents functions and documentation

--- gym_microgrid/agents/agent.py
+++ gym_microgrid/agents/agent.py
@@ -5,20 +5,26 @@ class Agent:
     def __init__(self):
         pass
 
-    def reset(self, ):
+    def reset(self):
         """
         Resets all agent buffers and discards unfinished episodes.
         TODO: Also set up the agent with respect to the environment (#actions, ...)
         """
         pass
 
-    def act(self, state: np.ndarray) -> np.ndarray:
+    def act(self, obs: np.ndarray) -> np.ndarray:
         """
         select an action with respect to the state
-        :param state:
+        this might update the internal state with respect to the history.
+        :param obs:
         :return:
         """
         pass
 
-    def observe(self, reward, terminated):
+    def observe(self, reward: float, terminated: bool):
+        """
+        :param reward:
+        :param terminated:
+        :return:
+        """
         pass
