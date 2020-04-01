0be15aedfd1fb0677be1898b454fdba50bc8a84d
Daniel Weber <weber@lea.uni-paderborn.de>
2020-04-01 15:54:00 +0200

    safeopt reward first try

--- gym_microgrid/agents/safeopt.py
+++ gym_microgrid/agents/safeopt.py
@@ -22,37 +22,55 @@ class SafeOptAgent(StaticControlAgent):
 
         self.episode_reward = None
         self.optimizer = None
+        self.inital_reward = None
         super().__init__(ctrls, observation_action_mapping)
 
     def reset(self):
         # reinstantiate kernel
         # self.kernel = type(self.kernel)(**self.kernel.to_dict())
-        self.kernel = GPy.kern.Matern32(input_dim=1, lengthscale=.01)
+        self.kernel = GPy.kern.Matern32(input_dim=1, lengthscale=.001)
 
         self.params.reset()
         self.optimizer = None
         self.episode_reward = 0
+        self.inital_reward = None
         return super().reset()
 
     def observe(self, reward, terminated):
         self.episode_reward += reward or 0
         if terminated:
+            # calculate MSE, divide Summed error by length of measurement
+            self.episode_reward = self.episode_reward / len(self.controllers['master'].history.df)
             # safeopt update step
             self.update_params()
-            # reset episode reward
-            self.episode_reward = 0
+            # reset for new episode
+            self.prepare_episode()
         # on other steps we don't need to do anything
 
     def update_params(self):
         if self.optimizer is None:
-            bounds = [(-0.1, 0.2)]
+            # First Iteration
+            self.inital_reward = self.episode_reward
+            # Norm for Safe-point
+            # y_0 = y_meas = initial_reward
+
+            bounds = [(0.02, 0.035)]
             noise_var = 0.05 ** 2
-            gp = GPy.models.GPRegression(np.array([self.params[:]]), np.array([[self.episode_reward]]), self.kernel,
+            gp = GPy.models.GPRegression(np.array([self.params[:]]),
+                                         np.array([[self.episode_reward / self.inital_reward]]), self.kernel,
                                          noise_var=noise_var)
             self.optimizer = SafeOptSwarm(gp, 0., bounds=bounds, threshold=1)
         else:
-            self.optimizer.add_new_data_point(self.params[:], self.episode_reward)
+
+            # y_safe = y_meas/y_norm
+            # y = y_meas/y_norm - 0.95*y_safe
+            # J_0 = y_meas - 0.95 * y_0
+            # J.append(2 - J_0 / J_0)
+            J = 1 / (self.episode_reward / self.inital_reward)
+
+            self.optimizer.add_new_data_point(self.params[:], J)
         self.params[:] = self.optimizer.optimize()
+        print(self.episode_reward / self.inital_reward)
 
     def render(self):
         plt.figure
