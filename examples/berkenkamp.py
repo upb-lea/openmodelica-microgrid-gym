a0c604a97287aaeddb8f34330eff7e18b8cdd3e0
Daniel Weber <weber@lea.uni-paderborn.de>
2020-04-01 15:52:38 +0200

    added reward function (Not finished)

--- examples/berkenkamp.py
+++ examples/berkenkamp.py
@@ -1,6 +1,8 @@
 import GPy
 
-delta_t = 1e-4
+from gym_microgrid.env import FullHistory
+
+delta_t = 5e-5
 V_dc = 1000
 nomFreq = 50
 nomVoltPeak = 230 * 1.414
@@ -13,8 +15,37 @@ from gym_microgrid.controllers import PI_params, DroopParams, MultiPhaseDQ0PIPIC
     MultiPhaseDQCurrentController, InverseDroopParams, PLLParams, MutableFloat
 from gym_microgrid.agents import StaticControlAgent, SafeOptAgent
 from gym_microgrid import Runner
+from gym_microgrid.common import *
 
 import numpy as np
+
+
+class reward:
+
+    def __init__(self, agent):
+        self.agent = agent
+
+    def rew_fun(self, obs):
+        # Vabc_master = [obs['lc1.capacitor1.v'][0], obs['lc1.capacitor2.v'][0], obs['lc1.capacitor3.v'][0]]
+        # print(agent.controllers['master'].history['SPVdq0'].iloc[-1][0])
+
+        # Measurements
+        Vabc_master = obs[[f'lc1.capacitor{i + 1}.v' for i in range(3)]].to_numpy()[0]
+        Iabc_master = obs[[f'lc1.inductor{i + 1}.i' for i in range(3)]].to_numpy()[0]
+
+        phase = agent.controllers['master'].history['phase'].iloc[-1]
+        Vdq0_master = abc_to_dq0(Vabc_master, phase)
+        Idq0_master = abc_to_dq0(Iabc_master, phase)
+
+        # Setpoints
+        VSPdq0_master = np.array(agent.controllers['master'].history['SPVdq0'].iloc[-1])
+        ISPdq0_master = np.array(agent.controllers['master'].history['SPVdq0'].iloc[-1])
+
+        error = np.sum((VSPdq0_master - Vdq0_master) ** 2, axis=0) + np.sum((ISPdq0_master - Idq0_master) ** 2, axis=0) \
+                + 0  # np.sum((Idq0_master - agent.controllers['master']._voltagePI.controllers)**4)
+
+        return error
+
 
 if __name__ == '__main__':
     # Bounds on the inputs variable
@@ -24,11 +55,12 @@ if __name__ == '__main__':
     kernel = GPy.kern.Matern32(input_dim=len(bounds), lengthscale=.01)
 
     # Define mutable parameters
-    mutable_params = dict(voltP=MutableFloat(25e-3), voltI=MutableFloat(60))
+    mutable_params = dict(voltP=MutableFloat(25e-3))  #, voltI=MutableFloat(60))
 
     ctrl = dict()
     # Voltage PI parameters for the current sourcing inverter
-    voltage_dqp_iparams = PI_params(kP=mutable_params['voltP'], kI=mutable_params['voltI'], limits=(-iLimit, iLimit))
+    # voltage_dqp_iparams = PI_params(kP=mutable_params['voltP'], kI=mutable_params['voltI'], limits=(-iLimit, iLimit))
+    voltage_dqp_iparams = PI_params(kP=mutable_params['voltP'], kI=60, limits=(-iLimit, iLimit))
     # Current PI parameters for the voltage sourcing inverter
     current_dqp_iparams = PI_params(kP=0.012, kI=90, limits=(-1, 1))
     # Droop of the active power Watt/Hz, delta_t
@@ -36,7 +68,7 @@ if __name__ == '__main__':
     # Droop of the reactive power VAR/Volt Var.s/Volt
     qdroop_param = DroopParams(QDroopGain, 0.002, nomVoltPeak)
     ctrl['master'] = MultiPhaseDQ0PIPIController(voltage_dqp_iparams, current_dqp_iparams, delta_t, droop_param,
-                                                 qdroop_param)
+                                                 qdroop_param, history=FullHistory())
 
     # Discrete controller implementation for a DQ based Current controller for the current sourcing inverter
     # Current PI parameters for the current sourcing inverter
@@ -58,10 +90,13 @@ if __name__ == '__main__':
                                      np.array([f'lcl1.capacitor{i + 1}.v' for i in range(3)]),
                                      np.zeros(3)]))
 
+    rew = reward(agent)
+
     env = gym.make('gym_microgrid:ModelicaEnv_test-v1',
+                   reward_fun=rew.rew_fun,
                    viz_mode='episode',
-                   max_episode_steps=100,
-                   model_path='grid.pll.fmu',
+                   max_episode_steps=300,
+                   model_path='grid.oneController.fmu',
                    model_input=['i1p1', 'i1p2', 'i1p3', 'i2p1', 'i2p2', 'i2p3'],
                    model_output=dict(lc1=[['inductor1.i', 'inductor2.i', 'inductor3.i'],
                                           ['capacitor1.v', 'capacitor2.v', 'capacitor3.v']],
