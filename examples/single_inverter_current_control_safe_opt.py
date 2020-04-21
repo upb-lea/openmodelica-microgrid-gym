c055daa6726b4eb2be0c8d3c2a51d10a4a6b052d
walli <wallscheid@lea.uni-paderborn.de>
2020-04-21 12:56:59 +0200

    correction of in-code comments in single_inverter_current_control_safe_opt.py

--- examples/single_inverter_current_control_safe_opt.py
+++ examples/single_inverter_current_control_safe_opt.py
@@ -1,7 +1,7 @@
 #####################################
-# Example using a FMU by OpenModelica and safeopt algorithm to find "optimal" controller parameters.
+# Example using a FMU by OpenModelica and SafeOpt algorithm to find optimal controller parameters
 # Simulation setup: Single inverter supplying 15 A d-current to an RL-load via a LC filter
-# Controller: PI current controller is optimized by SafeOpt
+# Controller: PI current controller gain parameters are optimized by SafeOpt
 
 
 import logging
@@ -19,31 +19,31 @@ from openmodelica_microgrid_gym.common import *
 import numpy as np
 import pandas as pd
 
-# Choose which controller parameters should be adjusted.
-# - adjust_Kp_only == True: 1D example: Only Kp ot the PI parameter is adjusted
-# - adjust_Ki_only == True: 1D example: Only Ki ot the PI parameter is adjusted
-# - adjust_Kp_and_Ki == True: 2D example: Kp and Ki are adjusted
-# ATTENTION! Choose only one parameter as TRUE!
+# Choose which controller parameters should be adjusted by SafeOpt.
+# - adjust_Kp_only == True: 1D example: Only the proportional gain Kp of the PI controller is adjusted
+# - adjust_Ki_only == True: 1D example: Only the integral gain Ki of the PI controller is adjusted
+# - adjust_Kp_and_Ki == True: 2D example: Kp and Ki are adjusted simultaneously
+# ATTENTION! Choose only one parameter as TRUE since the above selection represents separate optimization scenarios!
 adjust_Kp_only = True
 adjust_Ki_only = False
 adjust_Kp_and_Ki = False
-# summe aus 3 mal true = 1, sonst Fehler
+
+# Check if really only one simulation scenario was selected
 if sum([adjust_Kp_only, adjust_Ki_only, adjust_Kp_and_Ki]) is not 1:
-    raise ValueError("ATTENTION! Choose only one of the adjustable controller parameter as TRUE (l.30ff.)!")
+    raise ValueError("ATTENTION! Choose only one of the adjustable controller parameters as TRUE!")
 
 # Simulation definitions
 delta_t = 0.5e-4  # simulation time step size / s
 max_episode_steps = 300  # number of simulation steps per episode
 num_episodes = 15  # number of simulation episodes (i.e. SafeOpt iterations)
-v_DC = 700  # DC-link voltage / V; will be set as model parameter in the fmu
-nomFreq = 50  # grid frequency / Hz
+v_DC = 700  # DC-link voltage / V; will be set as model parameter in the FMU
+nomFreq = 50  # nominal grid frequency / Hz
 nomVoltPeak = 230 * 1.414  # nominal grid voltage / V
-iLimit = 30  # current limit / A
-iNominal = 20  # nominal current / A
-mu = 2  # Factor for barrier function (see below)
+iLimit = 30  # inverter current limit / A
+iNominal = 20  # nominal inverter current / A
+mu = 2  # factor for barrier function (see below)
 DroopGain = 40000.0  # virtual droop gain for active power / W/Hz
 QDroopGain = 1000.0  # virtual droop gain for reactive power / VAR/V
-
 i_ref = np.array([15, 0, 0])  # exemplary set point i.e. id = 15, iq = 0, i0 = 0 / A
 
 
@@ -51,22 +51,24 @@ def rew_fun(obs: pd.Series) -> float:
     """
     Defines the reward function for the environment. Uses the observations and setpoints to evaluate the quality of the
     used parameters.
-    Takes current measurements and setpoints so calculate the MSE control error and uses a logarithmic barrier function
-    in case of violating the current limit. Barrier adjustable using parameter mu.
+    Takes current measurements and setpoints so calculate the mean-root-error control error and uses a logarithmic
+    barrier function in case of violating the current limit. Barrier function is adjustable using parameter mu.
 
     :param obs: Observation from the environment (ControlVariables, e.g. currents and voltages)
     :return: Error as negative reward
     """
 
-    # Measurements: 3 Currents through the inductors of the inverter -> Control variables (CV)
+    # measurements: 3 currents through the inductors of the inverter -> control variables (CV)
     Iabc_master = obs[[f'lc1.inductor{i + 1}.i' for i in range(3)]].to_numpy()  # 3 phase currents at LC inductors
-    phase = obs[['master.phase']].to_numpy()[0]  # Phase from the master controller needed for transformation
+    phase = obs[['master.phase']].to_numpy()[0]  # phase from the master controller needed for transformation
 
-    # Setpoints
+    # setpoints
     ISPdq0_master = obs[['master.'f'SPI{k}' for k in 'dq0']].to_numpy()  # setting dq reference
     ISPabc_master = dq0_to_abc(ISPdq0_master, phase)  # convert dq set-points into three-phase abc coordinates
 
-    # control error = mean-root-error of reference minus measurements
+    # control error = mean-root-error (MRE) of reference minus measurements
+    # (due to normalization the control error is often around zero -> compared to MSE metric, the MRE provides
+    #  better, i.e. more significant,  gradients)
     # plus barrier penalty for violating the current constraint
     error = np.sum((np.abs((ISPabc_master - Iabc_master)) / iLimit) ** 0.5, axis=0) \
             + -np.sum(mu * np.log(1 - np.maximum(np.abs(Iabc_master) - iNominal, 0) / (iLimit - iNominal)), axis=0) \
@@ -78,33 +80,33 @@ def rew_fun(obs: pd.Series) -> float:
 if __name__ == '__main__':
     #####################################
     # Definitions for the GP
-    prior_mean = 2  # Mean factor of the GP prior mean which is multiplied with the first performance of the initial set
-    noise_var = 0.001 ** 2  # Measurement noise sigma_omega
-    prior_var = 0.1  # Prior variance of the GP
+    prior_mean = 2  # mean factor of the GP prior mean which is multiplied with the first performance of the initial set
+    noise_var = 0.001 ** 2  # measurement noise sigma_omega
+    prior_var = 0.1  # prior variance of the GP
 
     if adjust_Kp_only:
-        bounds = [(0.00, 0.03)]  # Bounds on the input variable Kp
-        lengthscale = [.01]  # Length scale for the parameter variation [Kp] for the GP
+        bounds = [(0.00, 0.03)]  # bounds on the input variable Kp
+        lengthscale = [.01]  # length scale for the parameter variation [Kp] for the GP
 
     # For 1D example, if Ki should be adjusted instead of Kp, choose adjust_Kp == False
     if adjust_Ki_only:
-        bounds = [(0, 300)]  # Bounds on the input variable Ki
-        lengthscale = [50.]  # Length scale for the parameter variation [Ki] for the GP
+        bounds = [(0, 300)]  # bounds on the input variable Ki
+        lengthscale = [50.]  # length scale for the parameter variation [Ki] for the GP
 
-    # For 2D example, choose Kp and Ki as mutable parameter (below) and define bounds and lengthscale for both of them
+    # For 2D example, choose Kp and Ki as mutable parameters (below) and define bounds and lengthscale for both of them
     if adjust_Kp_and_Ki:
         bounds = [(0.0, 0.03), (0, 300)]
         lengthscale = [.01, 50.]
 
-    # If the performance should not drop below the safe threshold, which is defined by the factor safe_threshold times
-    # the inital Performance: safe_threshold = 1.2 means, performance measurements for parameter-change are seen as
-    # unsafe, if the new measured performance drops below 20% of the initial performance of the initial safe (!)
+    # The performance should not drop below the safe threshold, which is defined by the factor safe_threshold times
+    # the initial performance: safe_threshold = 1.2 means. Performance measurements for optimization are seen as
+    # unsafe, if the new measured performance drops below 20 % of the initial performance of the initial safe (!)
     # parameter set
     safe_threshold = 2
 
     # The algorithm will not try to expand any points that are below this threshold. This makes the algorithm stop
     # expanding points eventually.
-    # is multiplied with the first performance of the initial set by the factor below:
+    # The following variable is multiplied with the first performance of the initial set by the factor below:
     explore_threshold = 2
 
     # Factor to multiply with the initial reward to give back an abort_reward-times higher negative reward in case of
@@ -117,14 +119,14 @@ if __name__ == '__main__':
     #####################################
     # Definition of the controllers
 
-    ctrl = dict()  # empty dictionary for the the controller(s)
+    ctrl = dict()  # empty dictionary for the controller(s)
 
     if adjust_Kp_only:
-        # mutable_params = Parameter (Kp of the current controller of the inverter) to change using safeopt algorithms in
-        # the safeopt agent to find an "optimal" PI controller parameter Kp using the safe inital parameter
+        # mutable_params = parameter (Kp gain of the current controller of the inverter) to be optimized using
+        # the SafeOpt algorithm
         mutable_params = dict(currentP=MutableFloat(10e-3))
 
-        # Define the PI parameters for the current Controller of the inverter
+        # Define the PI parameters for the current controller of the inverter
         current_dqp_iparams = PI_params(kP=mutable_params['currentP'], kI=115, limits=(-1, 1))
 
     # For 1D example, if Ki should be adjusted instead of Kp, choose adjust_Kp == False
@@ -132,32 +134,29 @@ if __name__ == '__main__':
         mutable_params = dict(currentI=MutableFloat(10))
         current_dqp_iparams = PI_params(kP=10e-3, kI=mutable_params['currentI'], limits=(-1, 1))
 
-    # For 2D example, choose Kp and Ki as mutable parameter
+    # For 2D example, choose Kp and Ki as mutable parameters
     if adjust_Kp_and_Ki:
         mutable_params = dict(currentP=MutableFloat(10e-3), currentI=MutableFloat(10))
         current_dqp_iparams = PI_params(kP=mutable_params['currentP'], kI=mutable_params['currentI'], limits=(-1, 1))
 
     # Define the droop parameters for the inverter of the active power Watt/Hz (DroopGain), delta_t (0.005) used for the
-    # filter and the nominal Frequenzy
-    # Droop controller used to calculate the virtual frequency drop due to load
+    # filter and the nominal frequency
+    # Droop controller used to calculate the virtual frequency drop due to load changes
     droop_param = DroopParams(DroopGain, 0.005, nomFreq)
 
     # Define the Q-droop parameters for the inverter of the reactive power VAR/Volt, delta_t (0.002) used for the
     # filter and the nominal voltage
-    # ToDo: needed? Due to class definition still inside
     qdroop_param = DroopParams(QDroopGain, 0.002, nomVoltPeak)
 
-    # Define a current sourcing inverter as master inverter using the pi and droop parameters from abouve
+    # Define a current sourcing inverter as master inverter using the pi and droop parameters from above
     ctrl['master'] = MultiPhaseDQCurrentSourcingController(current_dqp_iparams, delta_t, droop_param, qdroop_param,
                                                            undersampling=2)
 
     #####################################
-    # Definition of the agent
-    # Agent using the safeopt algorithm by Berkenkamp
-    # (https://arxiv.org/abs/1509.01066)
-    # as example
+    # Definition of the optimization agent
+    # The agent is using the SafeOpt algorithm by F. Berkenkamp (https://arxiv.org/abs/1509.01066) in this example
     # Arguments described above
-    # history is used to store results
+    # History is used to store results
     agent = SafeOptAgent(mutable_params,
                          abort_reward,
                          kernel,
@@ -172,16 +171,15 @@ if __name__ == '__main__':
                          )
 
     #####################################
-    # Definition of the enviroment using a FMU created by OpenModelica
+    # Definition of the environment using a FMU created by OpenModelica
     # (https://www.openmodelica.org/)
     # Using an inverter supplying a load
     # - using the reward function described above as callable in the env
-    # - viz_cols used to choose which measurement values should be displayed (here only the 3 currents across the
-    #   inductors of the inverters (alternatively also the grid frequency and the currents transformed by the agent to
-    #   dq0
-    # - inputs to the models are the connection points to the inverters (see user guide for more)
+    # - viz_cols used to choose which measurement values should be displayed (here, only the 3 currents across the
+    #   inductors of the inverters are plotted. Alternatively, the grid frequency and the currents transformed by
+    #   the agent to the dq0 system can be plotted if requested.)
+    # - inputs to the models are the connection points to the inverters (see user guide for more details)
     # - model outputs are the the 3 currents through the inductors and the 3 voltages across the capacitors
-    # ToDo: v_caps needed?
     env = gym.make('openmodelica_microgrid_gym:ModelicaEnv_test-v1',
                    reward_fun=rew_fun,
                    time_step=delta_t,
@@ -199,6 +197,6 @@ if __name__ == '__main__':
 
     #####################################
     # Execution of the experiment
-    # Using a runner to execute 10 different episodes
+    # Using a runner to execute 'num_episodes' different episodes (i.e. SafeOpt iterations)
     runner = Runner(agent, env)
     runner.run(num_episodes, visualise_env=True)
