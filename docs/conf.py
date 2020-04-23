2d60c74c2392f301069583aa975eb1667697ffd4
Stefan Heid <stefan.heid@upb.de>
2020-04-23 14:44:48 +0200

    removed m2r build dependency, added pyfmi build to travis.yml

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'm2r']
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,7 +52,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 # source_suffix = '.rst'
 
 # The master toctree document.
