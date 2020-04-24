7a8911adcc1bc6843a8ef536d63685591a6df45c
Stefan Heid <stefan.heid@upb.de>
2020-04-24 15:25:41 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
         "Documentation": "https://upb-lea.github.io/openmodelica-microgrid-gym/",
         "Source Code": "https://github.com/upb-lea/openmodelica-microgrid-gym",
     },
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
