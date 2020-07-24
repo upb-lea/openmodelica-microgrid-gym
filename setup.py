2c6863d7b1f8e4581fc3c3e860394b5991133ca8
Stefan Heid <stefan-heid@web.de>
2020-07-24 10:38:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux'        
     ],
     description="OpenModelica Microgrid Gym",
     install_requires=requirements,
