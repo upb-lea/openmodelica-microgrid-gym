6816f2067507f22cadb6f63bc010ab9bc99fbba9
Daniel Weber <weber@lea.uni-paderborn.de>
2020-10-15 10:14:58 +0200

    changed folder name and model paths

--- examples/basic_env.py
+++ examples/basic_env.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
                    model_input=['i1p1', 'i1p2', 'i1p3'],
                    max_episode_steps=None,
                    model_output=dict(lc1=['inductor1.i', 'inductor2.i', 'inductor3.i']),
-                   model_path='../OpenModelica_Microgrids/OpenModelica_Microgrids.Grids.Network.fmu')
+                   model_path='../omg_grid/omg_grid.Grids.Network.fmu')
 
     env.reset()
     for _ in range(1000):
