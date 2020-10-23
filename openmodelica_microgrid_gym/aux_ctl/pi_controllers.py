minor fix in reward calculation and some minor improvements

openmodelica_microgrid_gym/aux_ctl/pi_controllers.py
@@ -39,7 +39,7 @@ class PIController:
                 PI_Parameters provided during initialisation, clipped due to the defined limits
         """
 
-        self.integralSum = self.integralSum + (self._params.kI * error + self.windup_compensation) * self._ts
+        self.integralSum += (self._params.kI * error + self.windup_compensation) * self._ts
         output = self._params.kP * error + self.integralSum
         clipped = np.clip(output, *self._params.limits)
         self.windup_compensation = (output - clipped) * self._params.kB