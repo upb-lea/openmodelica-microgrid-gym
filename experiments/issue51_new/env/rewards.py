bc89e1808ab8170716d64be1ed9cfece66da7538
Daniel Weber <weber@lea.uni-paderborn.de>
2021-02-24 17:01:12 +0100

    - introduced exponent in reward to give hyperoptimization possibility to choose between e.g. MRE, MSE, MAE and between - created flag to turn of episode abort

--- experiments/issue51_new/env/rewards.py
+++ experiments/issue51_new/env/rewards.py
@@ -4,7 +4,7 @@ from typing import List
 
 
 class Reward:
-    def __init__(self, nom, lim, v_DC, gamma, nom_region=1.1, use_gamma_normalization=1):
+    def __init__(self, nom, lim, v_DC, gamma, det_run=False, nom_region=1.1, use_gamma_normalization=1):
         self._idx = None
         self.nom = nom
         self.lim = lim
@@ -15,6 +15,7 @@ class Reward:
         else:
             self.gamma = 0
         self.nom_region = nom_region
+        self.det_run = det_run
 
     def set_idx(self, obs):
         if self._idx is None:
@@ -56,14 +57,14 @@ class Reward:
                 rew = 1; if mess = SP
                 rew = 1/3; if error = SP-mess = 2*v_nom (worst case without braking out from nom area)
             """
-
             rew = np.sum((1 - np.abs(SP - mess) / (2 * self.nom)) * 2 * (1 - self.gamma) / 3 + (1 - self.gamma) / 3) / 3
 
-        elif any(np.abs(vabc_master) > self.lim):
+        elif any(np.abs(mess) > self.lim):
             """
             3rd area - outside valid area - above lim - possible if enough v_DC - DANGEROUS
             +-v_lim -> +-v_DC
-
+            
+            V1:
             @ SP = +v_nom AND mess = -v_DC:
                 rew = -1; if error = v_DC + v_nom -> Worst case, +v_nom wanted BUT -v_DC measured
             @ SP = -v_nom AND mess = -v_lim
@@ -71,13 +72,25 @@ class Reward:
                 rew -> -1 - 2/3*(1 - |lim - nom| / (nom+v_DC))
                 The latter fraction is quite small but leads to depending on the system less then 2/3 is
                 substracted and we have a gap to the 2nd area! :) 
+                
+            V2: None is returned to stop the episode (hint: in the env env.abort_reward is given back as reward(?)
+            
+            V3: rew = -1
             """
 
-            rew = np.sum(
-                (1 - np.abs(SP - mess) / (self.nom + self.v_DC)) * 2 * (1 - self.gamma) / 3 - (1 - self.gamma)) / 3
+            # V1:
+            # rew = np.sum(
+            #    (1 - np.abs(SP - mess) / (self.nom + self.v_DC)) * 2 * (1 - self.gamma) / 3 - (1 - self.gamma)) / 3
 
+            # V2:
             # if return -> rew = None and in env abort_reward is given to agent
-            # return
+            if self.det_run:
+                return -(1 - self.gamma)
+            else:
+                return
+
+            # V3:
+            # rew = (1 - gamma)
 
         # elif any(np.abs(vabc_master) > v_DC):
         #    rew = (1-gamma)
