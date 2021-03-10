b6b6bc69ccd5e9aba323cd4559a2f5df09efeacd
webbah <weber@lea.uni-paderborn.de>
2021-03-10 13:53:57 +0100

    - minor

--- experiments/hp_tune/env/vctrl_single_inv.py
+++ experiments/hp_tune/env/vctrl_single_inv.py
@@ -19,7 +19,7 @@ from openmodelica_microgrid_gym.net import Network
 from openmodelica_microgrid_gym.util import RandProcess
 from gym.envs.registration import register
 
-folder_name = 'Testrun_mit_action_delay'
+folder_name = 'Obs_fix_hyperopt'
 # experiment_name = 'DDPG_VC_Reward_MRE_reward_NOT_NORMED'
 experiment_name = 'plots'
 timestamp = datetime.now().strftime(f'_%Y.%b.%d_%X')
