1e4660c36edc54acca9f8cf576524a8f9151ed2e
Daniel Weber <weber@lea.uni-paderborn.de>
2021-04-26 14:40:23 +0200

    minor

--- experiments/hp_tune/env/vctrl_single_inv.py
+++ experiments/hp_tune/env/vctrl_single_inv.py
@@ -19,7 +19,7 @@ from openmodelica_microgrid_gym.net import Network
 from openmodelica_microgrid_gym.util import RandProcess
 from gym.envs.registration import register
 
-folder_name = 'Master_V_ctrl_DDPG_TEST'
+folder_name = 'Master_V_ctrl_DDPG'
 # experiment_name = 'DDPG_VC_Reward_MRE_reward_NOT_NORMED'
 experiment_name = 'plots'
 timestamp = datetime.now().strftime(f'_%Y.%b.%d_%X')
