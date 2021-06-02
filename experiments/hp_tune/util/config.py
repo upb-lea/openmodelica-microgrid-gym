ba2f6e76c19bc17b36f49ba3583b2d8cfa11bb8e
Daniel Weber <weber@lea.uni-paderborn.de>
2021-06-02 15:22:11 +0200

    - renaming Study for PC2

--- experiments/hp_tune/util/config.py
+++ experiments/hp_tune/util/config.py
@@ -1,7 +1,7 @@
 cfg = dict(lea_vpn_nodes=['lea-skynet', 'lea-picard', 'lea-barclay',
                           'lea-cyberdyne', 'webbah-ThinkPad-L380'],
 
-           STUDY_NAME='PC2Debug',  # 'DDPG_MRE_sqlite_PC2',
+           STUDY_NAME='PC2_DDPG_MRE_V_only',
            meas_data_folder='Json_to_MonogDB/',
            MONGODB_PORT=12001
            )
