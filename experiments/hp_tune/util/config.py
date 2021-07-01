1c590189104e8ee9b9a8d6ff4c577060df4bd53a
Daniel Weber <weber@lea.uni-paderborn.de>
2021-07-01 17:32:01 +0200

    - prepared study 14, wrong config name

--- experiments/hp_tune/util/config.py
+++ experiments/hp_tune/util/config.py
@@ -1,7 +1,7 @@
 cfg = dict(lea_vpn_nodes=['lea-skynet', 'lea-picard', 'lea-barclay',
                           'lea-cyberdyne', 'webbah-ThinkPad-L380', 'LEA_WORK35'],
 
-           STUDY_NAME='PC2_DDGP_Vctrl_single_inv_14_from_Study_13_HPs_trail_651_smaller_LR',
+           STUDY_NAME='PC2_DDGP_Vctrl_single_inv_14',
            meas_data_folder='Json_to_MonogDB/',
            MONGODB_PORT=12001,
            loglevel='test',
