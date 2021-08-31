71cd4456c56644bb18a2de70bff58d43cdd49b74
Daniel Weber <weber@lea.uni-paderborn.de>
2021-08-31 15:06:35 +0200

    - added testcase load data 10 s doing random loadsteps all the time including double loadstep bug - added mean to reset to not drift again to old load value

--- openmodelica_microgrid_gym/util/randproc.py
+++ openmodelica_microgrid_gym/util/randproc.py
@@ -33,8 +33,12 @@ class RandProcess:
         """
         if initial is None:
             self._last = np.random.uniform(low=self.bounds[0], high=self.bounds[1])
+            self.proc.mean = self._last
+            # self.reserve = self._last
         else:
             self._last = initial
+            self.proc.mean = self._last
+            #self.reserve = self._last
         self._last_t = 0
         self._reserve = None
 
