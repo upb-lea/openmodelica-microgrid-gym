830c5119444c90d446c507819e0d8091a683bab5
Daniel Weber <weber@lea.uni-paderborn.de>
2021-09-06 09:43:33 +0200

    - give possibility to turn on/off past observations (v_dq0) as features - changed loadstep to one per train_episode - config for best 22 plus past obs feature

--- experiments/hp_tune/util/config.py
+++ experiments/hp_tune/util/config.py
@@ -3,12 +3,12 @@ cfg = dict(lea_vpn_nodes=['lea-skynet', 'lea-picard', 'lea-barclay',
 
            # STUDY_NAME='PC2_TD3_Vctrl_single_inv_2',
            # STUDY_NAME='PC2_DDPG_Vctrl_single_inv_23_added_Past_vals',
-           STUDY_NAME='PC2_DDPG_Vctrl_single_inv_22_best_pastVal_HPO',
+           STUDY_NAME='PC2_DDPG_Vctrl_single_inv_22_best_nopastVal_newTestcase',
            meas_data_folder='Json_to_MonogDB/',
            MONGODB_PORT=12001,
            loglevel='test',
            is_dq0=True,
            train_episode_length=2881,  # defines when in training the env is reset e.g. for exploring starts,
            # nothing -> Standard FeatureWrapper; past -> FeatureWrapper_pastVals; future -> FeatureWrapper_futureVals
-           env_wrapper='past'
+           env_wrapper='No_past'
            )
