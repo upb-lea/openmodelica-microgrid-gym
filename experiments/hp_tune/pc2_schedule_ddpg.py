73e9f74902dcf424762e21d4ab4b04ef58c11d0a
Daniel Weber <weber@lea.uni-paderborn.de>
2021-09-09 10:38:31 +0200

    - back to HPO 24 + 400 Cores for PC2

--- experiments/hp_tune/pc2_schedule_ddpg.py
+++ experiments/hp_tune/pc2_schedule_ddpg.py
@@ -14,7 +14,7 @@ from experiments.hp_tune.util.config import cfg
 
 # config
 USER = os.getenv('USER')
-ALLOWED_MAX_CPU_CORES = 256  # 512
+ALLOWED_MAX_CPU_CORES = 400  # 512
 STUDY_NAME = cfg['STUDY_NAME']
 DB_NAME = 'optuna'
 # resources request
