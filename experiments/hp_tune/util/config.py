825fea0293d6b74e5890acb8406cb9bb114c1999
Daniel Weber <weber@lea.uni-paderborn.de>
2021-09-20 16:26:04 +0200

    - config change

--- experiments/hp_tune/util/config.py
+++ experiments/hp_tune/util/config.py
@@ -3,9 +3,9 @@ cfg = dict(lea_vpn_nodes=['lea-skynet', 'lea-picard', 'lea-barclay',
 
            # STUDY_NAME='PC2_TD3_Vctrl_single_inv_2',
            # STUDY_NAME='PC2_DDPG_Vctrl_single_inv_23_added_Past_vals',
-           STUDY_NAME='PC2_DDPG_Vctrl_single_inv_HPO_noI_term_study_25',
+           STUDY_NAME='PC2_DDPG_Vctrl_single_inv_HPO_noI_term_study_25_withoutSinCos',
            # STUDY_NAME='PC2_DDPG_Vctrl_single_inv_PastValHPO_without_phaseFeature',
-           meas_data_folder='Json_to_MonogDB_study_24/',
+           meas_data_folder='Json_to_MonogDB_study_25/',
            MONGODB_PORT=12001,
            loglevel='test',
            is_dq0=True,
@@ -13,3 +13,4 @@ cfg = dict(lea_vpn_nodes=['lea-skynet', 'lea-picard', 'lea-barclay',
            # nothing -> Standard FeatureWrapper; past -> FeatureWrapper_pastVals; future -> FeatureWrapper_futureVals
            env_wrapper='no-I-term'
            )
+# Future_10Rvals_study_withNormalization
