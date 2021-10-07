6cb8fd7f35f98fb62e97c8e7bb2a1090d58dfb52
Daniel Weber <weber@lea.uni-paderborn.de>
2021-10-07 11:27:57 +0200

    - add a few no I term samples

--- experiments/GEM/util/config.py
+++ experiments/GEM/util/config.py
@@ -8,5 +8,5 @@ cfg = dict(lea_vpn_nodes=['lea-skynet', 'lea-picard', 'lea-barclay',
            train_episode_length=2881,  # defines when in training the env is reset e.g. for exploring starts,
            # nothing -> Standard FeatureWrapper; past -> FeatureWrapper_pastVals; future -> FeatureWrapper_futureVals
            # I-controller -> DDPG as P-term + standard I-controller; no-I-term -> Pure DDPG without integrator
-           env_wrapper='past'
+           env_wrapper='no-I-term'
            )
