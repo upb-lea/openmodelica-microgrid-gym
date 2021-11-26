e952f7ba19345ec42d2c9971c8f5d305a7bb71eb
Daniel Weber <weber@lea.uni-paderborn.de>
2021-11-26 10:37:29 +0100

    - test 2_213 minor

--- experiments/P10/viz/Com_models_pc2_P10.py
+++ experiments/P10/viz/Com_models_pc2_P10.py
@@ -48,7 +48,7 @@ class CallbackList(list):
 show_plots = True
 save_results = False
 
-folder_name = 'saves/P10_I_term_1/213_deterministic'
+folder_name = 'saves/P10_I_term_2/213_deterministic'
 number_past_vals = [20]
 wrapper = ['past']  # ['past', 'future', 'no-I-term', 'I-controller']
 model_path = 'experiments/P10/trained_models/2_213/'
